b0010e1ca473b47bc56b66fd9f273ef5d30acdca
Brian Oxley <boxley@thoughtworks.com>
2016-11-12 06:58:15 -0600

    Revert "Use pipelined function to run tests"
    This reverts commit f034fa014e2c4a201b3e76af4068a68569a6e812.
    
    Losing the backtrace when pipelining.

--- punit_testing.sql
+++ punit_testing.sql
@@ -1,17 +1,6 @@
-DROP TYPE t_punit_test_results;
-CREATE OR REPLACE TYPE t_punit_test_result AS OBJECT (
-  testee VARCHAR2(61),
-  test_result INT,
-  exception_code NUMBER,
-  cause VARCHAR2(512),
-  trace VARCHAR2(5000));
-CREATE TYPE t_punit_test_results AS TABLE OF t_punit_test_result;
-/
 CREATE OR REPLACE PACKAGE PUNIT_TESTING IS
-  PROCEDURE run_tests(package_name STRING, die_if_failed BOOLEAN DEFAULT true);
-  FUNCTION do_run_tests(package_name string, test_name_pattern string)
-    RETURN t_punit_test_results PIPELINED;
-  PROCEDURE disable_test(reason STRING);
+  PROCEDURE run_tests(package_name STRING, die_if_failed boolean DEFAULT true);
+  PROCEDURE disable_test(reason string);
   PROCEDURE assert_equals(expected INT, actual INT);
 END PUNIT_TESTING;
 /
@@ -58,36 +47,9 @@ CREATE OR REPLACE PACKAGE BODY PUNIT_TESTING IS
         RETURN to_char(diff / 100, 'FM990.00');
     END to_hundreds_of_second;
 
-  FUNCTION do_run_tests(package_name string, test_name_pattern string)
-    RETURN t_punit_test_results PIPELINED IS
-      testee VARCHAR2(61);
-      test_result t_punit_test_result := t_punit_test_result(NULL, NULL, 0, NULL, NULL);
-    BEGIN
-      FOR p IN (SELECT procedure_name
-          FROM ALL_PROCEDURES
-          WHERE object_name = package_name
-          AND procedure_name LIKE test_name_pattern)
-        LOOP
-          testee := package_name || '.' || p.procedure_name;
-          BEGIN
-            EXECUTE IMMEDIATE 'BEGIN ' || testee || '; END;';
-            test_result := t_punit_test_result(testee, 0, 0, NULL, NULL);
-          EXCEPTION
-            WHEN disabled_test THEN
-              test_result := t_punit_test_result(testee, 1, SQLCODE, SQLERRM, NULL);
-            WHEN assertion_error THEN
-              test_result := t_punit_test_result(testee, 2, SQLCODE, SQLERRM, NULL);
-            WHEN OTHERS THEN
-              test_result := t_punit_test_result(testee, 3, SQLCODE, SQLERRM,
-                  DBMS_UTILITY.format_error_backtrace());
-          END;
-          PIPE ROW(test_result);
-        END LOOP;
-        RETURN;
-    END do_run_tests;
-
   PROCEDURE run_tests(package_name string, die_if_failed boolean) IS
       start_time timestamp  := systimestamp;
+      testee VARCHAR2(61);
       run int := 0;
       passed int := 0;
       failed int := 0;
@@ -95,31 +57,35 @@ CREATE OR REPLACE PACKAGE BODY PUNIT_TESTING IS
       skipped int := 0;
     BEGIN
       DBMS_OUTPUT.put_line('Running ' || package_name);
-
-      FOR t IN (SELECT * FROM TABLE(do_run_tests(package_name, 'TEST_%')))
+      FOR p IN (SELECT procedure_name
+          FROM ALL_PROCEDURES
+          WHERE object_name = package_name
+          AND procedure_name LIKE 'TEST_%')
         LOOP
           run := run + 1;
-          CASE t.test_result
-            WHEN 0 THEN
-              passed := passed + 1;
-              DBMS_OUTPUT.put_line(unistr('\2713') || ' ' || t.testee || ' passed.');
-            WHEN 1 THEN
+          testee := package_name || '.' || p.procedure_name;
+          BEGIN
+            EXECUTE IMMEDIATE 'BEGIN ' || testee || '; END;';
+            passed := passed + 1;
+            DBMS_OUTPUT.put_line(unistr('\2713') || ' ' || testee || ' passed.');
+          EXCEPTION
+            WHEN disabled_test THEN
               skipped := skipped + 1;
-              DBMS_OUTPUT.put_line('- ' || t.testee || ' skipped: ' || t.cause);
-            WHEN 2 THEN
+              DBMS_OUTPUT.put_line('- ' || testee || ' skipped: ' || SQLERRM);
+            WHEN assertion_error THEN
               IF (die_if_failed) THEN
-                raise_application_error(t.exception_code, t.cause);
+                RAISE;
               END IF;
               failed := failed + 1;
-              DBMS_OUTPUT.put_line(unistr('\2717') || ' ' || t.testee || ' failed: ' || t.cause);
-            WHEN 3 THEN
+              DBMS_OUTPUT.put_line(unistr('\2717') || ' ' || testee || ' failed: ' || SQLERRM);
+            WHEN OTHERS THEN
               IF (die_if_failed) THEN
-                raise_application_error(t.exception_code, t.cause);
+                RAISE;
               END IF;
               errored := errored + 1;
-              DBMS_OUTPUT.put_line('? ' || t.testee || ' errored: ' || t.cause);
+              DBMS_OUTPUT.put_line('? ' || testee || ' errored: ' || SQLERRM);
               DBMS_OUTPUT.put_line(DBMS_UTILITY.format_error_backtrace());
-          END CASE;
+          END;
         END LOOP;
 
         DBMS_OUTPUT.put_line('Tests run: ' || run || ', Failures: ' || failed || ', Errors: ' || errored || ', Skipped: ' || skipped || ', Time elapsed: ' || to_hundreds_of_second(systimestamp, start_time) || ' sec - in ' || package_name);
